6e0e8d5073878a0c989a100bea828aa6bb3e2773
Cynda <65635932+CyndaquilDAC@users.noreply.github.com>
2023-09-18 19:11:48 -0500

    yea

--- scripts/camerafollow.gd
+++ scripts/camerafollow.gd
@@ -1,20 +1,19 @@
-extends Node2D
+extends Camera2D
 
+var sprite:Node2D;
 var xMove:float
 var yMove:float
 var moveMulti:float = 2;
 var stammy:float = 1;
 
-var sprite:AnimatedSprite2D;
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	stammy = 1;
-	sprite = get_node("AnimatedSprite2D");
-	sprite.play("idle");
+	#sprite = get_parent().get_node("Roomo");
 	pass # Replace with function body.
 
 
+#god this sucks so bad im not used to this at alll
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if Input.is_action_pressed("run") && stammy > 0:
@@ -40,12 +39,4 @@ func _process(delta):
 		
 	position.x += xMove * moveMulti;
 	position.y += yMove * moveMulti;
-	
-	if xMove == 0 && yMove == 0 && sprite.animation == "walk" && sprite.animation != "idle":
-		sprite.play("idle");
-	else:
-		if xMove != 0 || yMove != 0:
-			if sprite.animation != "walk":
-				sprite.play("walk");
-	
 	pass
